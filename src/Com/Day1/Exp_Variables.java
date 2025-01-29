1bd85bc9bc490195e3c6ff0a0608b141841694ef
padmaja <padmaja7601@gmail.com>
2025-01-28 18:05:44 -0600

    Core_java_Learning

---
+++ src/Com/Day1/Exp_Variables.java
@@ -0,0 +1,23 @@
+package Com.Day1;
+
+public class Exp_Variables {
+    public static void main(String[] args) {
+
+
+       // public class Variables_DataTypes {
+        int nummber = 10;
+        float number_FLoat = 2.34f;
+        double number_Double = 1.2345;
+        char character = 'A';
+        boolean number_boolean = true;
+
+        System.out.println("Integer: " + nummber);
+        System.out.println("Float value: " + number_FLoat);
+        System.out.println("Double value: " + number_Double);
+        System.out.println("Character value: " + character);
+        System.out.println("Boolean value: " + number_boolean);
+
+
+
+    }
+}
